e5897359ce811c7284348fe4465441de59057714
kimizuka <f.kimizuka@gmail.com>
2025-12-17 09:15:22 +0900

    データ永続化

--- open_notebook/config.py
+++ open_notebook/config.py
@@ -1,7 +1,9 @@
 import os
 
 # ROOT DATA FOLDER
-DATA_FOLDER = "./data"
+# Use DATA_FOLDER env var if set (e.g., /mydata for Cloud Run with GCS mount)
+# Otherwise default to ./data for local development
+DATA_FOLDER = os.environ.get("DATA_FOLDER", "./data")
 
 # LANGGRAPH CHECKPOINT FILE
 sqlite_folder = f"{DATA_FOLDER}/sqlite-db"
